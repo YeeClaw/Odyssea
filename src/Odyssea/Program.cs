ed20a164b3ade17e3335b562c82b8db20fb821e5
YeeClaw <austin@jcolt.net>
2020-12-29 17:27:29 -0700

    First commit

---
+++ src/Odyssea/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Odyssea
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
